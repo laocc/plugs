b77d1c9c175e768c6f0a185ef085353f76f270ae
Administrator <fazo@qq.com>
2016-11-23 15:03:06 +0800

    init

---
+++ kernel/libraries/scws_new.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/11/23
+ * Time: 14:54
+ */
+class scws_new
+{
+
+    public function __construct()
+    {
+    }
+
+    public function set_charset($chart)
+    {
+
+    }
+
+    public function set_ignore($bool)
+    {
+
+    }
+
+    public function set_duality($bool)
+    {
+
+    }
+
+    public function set_multi($bool)
+    {
+
+    }
+
+    public function set_dict($file)
+    {
+
+    }
+
+    public function send_text($string)
+    {
+
+    }
+
+    public function get_result()
+    {
+        return [];
+    }
+}
